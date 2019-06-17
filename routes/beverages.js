24ccaa1c71e0eb1d88761e31e4dfe51b889f919d
Arthur Baudel <arthurbaudel75.com>
2019-06-17 08:54:04 +0200

    beverages.js to be checked

--- routes/beverages.js
+++ routes/beverages.js
@@ -1,27 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const connection = require ('../conf');
-
-// body-parser module
-
-const bodyParser = require('body-parser');
-router.use(bodyParser.urlencoded({
-	extended: true
-}));
-
-router.use(bodyParser.json());
+const connection = require ("../conf");
 
 router.get('/', (req, res) => {
 	
-	connection.query('SELECT idBeverages, bevName, CAST(FLOOR(bevPrice_ht * taxValue * 100) / 100 AS DECIMAL(16,2)) AS bevPriceTTC ' + 
+	connection.query('SELECT idBeverages, bevName, CAST(FLOOR(bevPriceHt * taxValue * 100) / 100 AS DECIMAL(16,2)) AS bevPriceTTC ' + 
 	'FROM beverages JOIN tax ON beverages.idTax = tax.idTax', (err, results) => {
 
 		if (err){
-			// answer given to the user in case of an error
 			res.status(500).send('Erreur lors de la rcupération de la boisson');
-		
 		} else {
-			// if there is no errors, the result is returned in the JSON format
 			res.json(results);
 		};
 	});
@@ -34,14 +22,9 @@ router.post('/', (req, res) => {
 
 	// connection into the database and insert beverages
 	connection.query('INSERT INTO beverages SET ?', beveragesCreate, (err, results) => {
-
 		if (err) {
-			console.log(err)
-			// if an error occurs, we warn the user
 			res.status(500).send('Erreur lors de la commande de votre boisson');
-
 		} else {
-			// If everything is good, send an OK status
 			res.sendStatus(200);
 		};
 	});
@@ -54,12 +37,9 @@ router.put('/', (req, res) => {
 	const beveragesUpdate = req.body; // Get all data
   
 	connection.query('UPDATE beverages SET ? WHERE idBeverages = ?', [beveragesUpdate, id], err => {
-  
 	  if (err) {
-		
 		res.status(500).send('Erreur lors de la modification de la boisson');
 	  	} else {
-  
 			res.sendStatus(200);
 	  	};
 	});
@@ -70,13 +50,9 @@ router.delete('/', (req, res) => {
 	const id = req.body.idBeverages;
   
 	connection.query('DELETE FROM beverages WHERE idBeverages = ?', [id], err => {
-  
 		if (err) {
-			
 			res.status(500).send("Erreur lors de la suppression d'un dessert");
-	  
 		} else {
-  
 		 res.sendStatus(200);
 	   };
 	});
