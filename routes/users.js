1c8e1c505da2d6d1e9219d7bf929ae02dd67378d
Triple <sebastien.andrys@gmail.com>
2019-06-20 16:19:59 +0200

    users routes created and working with postman

---
+++ routes/users.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const router = express.Router();
+const connection = require("../conf");
+
+router.get("/", (req, res) => {
+  connection.query('SELECT * FROM users', (err, results) => {
+    if (err) {
+      res.status(500).send("Erreur lors de l'affichage des informations de l'utilisateur");
+    } else {
+      res.json(results);
+    };
+  });
+});
+
+router.post("/", (req, res) => {
+  const userCreate = req.body;
+  
+  connection.query('INSERT INTO users SET ?', userCreate, (err, results) => {
+    if (err) {
+      res.status(500).send("Erreur lors de la création de l'utilisateur");
+    } else {
+      res.sendStatus(200);
+    };
+  });
+});
+
+router.put('/', (req, res) => {
+  const firstNameUser = req.body.firstname;
+  const lastNameUser = req.body.lastname;
+  const userUpdate = req.body;
+
+  connection.query('UPDATE users SET ? WHERE firstname = ? AND lastname = ?', [userUpdate, firstNameUser, lastNameUser], err => {
+    if (err) {
+      res.status(500).send("Erreur lors de la mise à jour de l'utilisateur");
+    } else {
+      res.sendStatus(200);
+    };
+  });
+});
+
+router.delete("/", (req, res) => {
+  const firstNameUser = req.body.firstname;
+  const lastNameUser = req.body.lastname;
+  const userDelete = req.query.user;
+
+  connection.query('DELETE FROM users WHERE firstname = ? AND lastname = ?', [userDelete, firstNameUser, lastNameUser], (err, results) => {
+    if (err) {
+      res.status(500).send('Erreur lors de la suppression de la pizza');
+    } else {
+      res.sendStatus(200);
+    };
+  });
+});
+
+module.exports = router;
