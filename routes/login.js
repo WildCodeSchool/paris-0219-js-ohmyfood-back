a53276c464b473cf0ebe1b8ccfaa54bc45c71375
Triple <sebastien.andrys@gmail.com>
2019-07-01 13:09:05 +0200

    login updated for authent jwt

--- routes/login.js
+++ routes/login.js
@@ -1,14 +1,70 @@
 const express = require("express");
 const router = express.Router();
 const connection = require("../conf");
+const jwt = require("jsonwebtoken");
+const jwtSecret = require("../jwtSecret");
 
 router.post("/", (req, res) => {
-  const authInformation = req.body;
-    if (err) {
-      res.status(500).send('Erreur lors de la récupération de vos informations');
+  const userData = req.body;
+  const userMail = req.body.mail;
+  const userPssw = req.body.password;
+
+  connection.query(`SELECT mail FROM users WHERE mail = '${userMail}'`, (err, results) => {
+
+    if (results.length === 0) {
+      res.status(401).send("Vous n'avez pas de compte");
     } else {
-      res.sendStatus(200);
-    };
-});
+      connection.query(`SELECT password FROM users WHERE mail = '${userMail}' AND password = '${userPssw}'`, (err, results) => {
+        if(results.length === 0) {
+          console.error(err);
+          res.status(401).send("Wrong password");
+        } else {
+          console.log("user recognized");
+          const token = jwt.sign(userData, jwtSecret, (err, token) => {
+            res.json({
+              token
+            })
+          })
+          res.header("Access-Control-Expose-Headers", "x-access-token");
+          res.set("x-access-token", token);
+          console.log(res.header)
+          res.status(200);
+        }
+      })
+    }
+  })
+})
+
+// vérifier le token pour les pages protégées 
+
+router.post("/protected", (req, res, next) => {
+  console.log('protectCall')
+  const token = verifToken(req);
+  const objectTests = { //data appelées par la bdd 
+    test: 'ok',
+  }
+  jwt.verify(token, jwtSecret, (err, decoded) => {
+    if(err) {
+      console.log(err);
+      return res.status(401).send({mess: "Vous n'avez pas accès aux données"});
+    }
+    console.log('decode',decoded);
+    return res.status(200).send({mess: 'User Datas', objectTests });
+  })
+})
+
+// Verify token function
+
+const verifToken = req => {
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.split(" ")[0] === "Bearer"
+  ) {
+    return req.headers.authorization.split(" ")[1];
+  } else if (req.query && req.query.token) {
+    return req.query.token;
+  }
+  return null;
+};
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const connection = require("../conf");
 
+
 router.get("/", (req, res) => {
   connection.query('SELECT * FROM users', (err, results) => {
     if (err) {
@@ -13,16 +14,34 @@ router.get("/", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-  const userCreate = req.body;
-  
-  connection.query('INSERT INTO users SET ?', userCreate, (err, results) => {
+
+  const userData = req.body;
+  const userMail = req.body.mail
+  console.log('userMail', userData.mail)
+
+  connection.query(`SELECT mail FROM users WHERE mail = '${userMail}'`, (err, results) => {
     if (err) {
-      res.status(500).send("Erreur lors de la création de l'utilisateur");
-    } else {
-      res.sendStatus(200);
-    };
+      console.log(err);
+      res.status(500).send("Erreur lors de la vérification de l'email");
+    } else if (results.length > 0) {
+      console.log("L'email existe déjà")
+      res.status(409, 'L\'email existe déja dans la base de donnée')
+    } 
+    else {
+      connection.query('INSERT INTO users SET ?', [userData], (err, results) => {
+        if (err) {
+          console.log(err);
+          res.status(500).send("Erreur lors de la création de l'utilisateur");
+        }
+        else {
+          console.log(results)
+          res.sendStatus(200)
+        } 
+      });
+    } 
   });
 });
+
 
 router.put('/', (req, res) => {
   const firstNameUser = req.body.firstname;
@@ -50,4 +69,4 @@ router.delete("/", (req, res) => {
   });
 });
 
-module.exports = router;
+module.exports = router
