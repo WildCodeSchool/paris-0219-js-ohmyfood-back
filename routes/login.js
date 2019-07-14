aebdf54efc7cee0d0a4b6722e4b203456dfc2cf7
Marc Bernini <bernini.marc@gmail.com>
2019-07-14 14:27:32 +0200

    Add logic to use bcrypt to hash user password when he creates his account. Use bcrypt also to compare password in database and from user's request to check if it match

--- routes/login.js
+++ routes/login.js
@@ -3,36 +3,46 @@ const router = express.Router();
 const connection = require("../conf");
 const jwt = require("jsonwebtoken");
 const jwtSecret = require("../jwtSecret");
+const bcrypt = require("bcrypt");
 
 router.post("/", (req, res) => {
   const userData = req.body;
   const userPssw = req.body.password;
   const userMail = req.body.mail;
-  connection.query(`SELECT * FROM users WHERE mail = '${userMail}' AND password = '${userPssw}'`, (err, results) => {
+
+  connection.query(`SELECT password FROM users WHERE mail = '${userMail}'`, (err, results) => {
     if (results.length === 0) {
       res.status(401).send("Vous n'avez pas de compte");
+    }
+
+    if (err) {
+      res.send('error ' + err);
+
     } else {
-      connection.query(`SELECT firstname, lastname, mail, password FROM users WHERE mail = '${userMail}' AND password = '${userPssw}'`, (err, results) => {
-        if(results[0].password.length === 0) {
-          console.error(err);
-          res.status(401).send("Wrong password");
-        } else {
-          console.log("user recognized");
-          const token = jwt.sign(userData, jwtSecret, (err, token) => {
-              res.json({
-                token,
-                'userMail': results['0'].mail,
-                'userFirstName': results['0'].firstname,
-                'userLastName': results['0'].lastname,
-                'userRight': results['0'].userRight,
-                'userId': results['0'].idUsers
-              })
-          })
-          res.header("Access-Control-Expose-Headers", "x-access-token");
-          res.set("x-access-token", token);
-          res.status(200);
-        }
-      })
+
+      // User bcrypt to compare input password and password in database OR check password if it's not crypt
+      if (bcrypt.compareSync(userPssw, results[0].password) || userPssw === results[0].password) {
+        connection.query(`SELECT firstname, lastname, mail, password FROM users WHERE mail = '${userMail}' AND password = '${results[0].password}'`, (err, results) => {
+          if(err) {
+            res.send('error ', err);
+          } else {
+            console.log("user recognized");
+            const token = jwt.sign(userData, jwtSecret, (err, token) => {
+                res.json({
+                  token,
+                  'userMail': results['0'].mail,
+                  'userFirstName': results['0'].firstname,
+                  'userLastName': results['0'].lastname,
+                  'userRight': results['0'].userRight,
+                  'userId': results['0'].idUsers
+                })
+            })
+            res.header("Access-Control-Expose-Headers", "x-access-token");
+            res.set("x-access-token", token);
+            res.status(200);
+          }
+        })
+      }  
     }
   })
 })
