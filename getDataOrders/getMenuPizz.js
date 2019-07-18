1276f5fa996531a5b87a109b9acb82cf9c37a83b
Marc Bernini <bernini.marc@gmail.com>
2019-07-18 19:07:58 +0200

    Remove map because useless and change sqlRequest to optimize results

--- getDataOrders/getMenuPizz.js
+++ getDataOrders/getMenuPizz.js
@@ -1,44 +1,23 @@
 const connection = require("../conf");
 
-// ---------------------------------------GET ID FROM MENU----------------------------- 
-
-const  sqlRequestMenuTable = orderId => 
-    `SELECT idOrders, idPizzas, idBeverages, idDesserts, menuPizzPrice, menuQuantity FROM menu ` + 
-    `WHERE menu.idOrders = ${orderId} AND idPizzas IS NOT NULL`;
-
-const menuPizzasData = orderId => {
-    return new Promise((resolve, reject) => {
-        connection.query(sqlRequestMenuTable(orderId), (err, results) => {
-            if (err) reject([err, 1]);
-            resolve(results);
-        });  
-    });
-};
-
 // ------------------------------------------GET DETAIL MENU------------------
 
-const sqlRequestDetailsMenu = menuPizza => 
-    `SELECT pizzName, bevName, dessName FROM menu ` +
-    `JOIN pizzas ON pizzas.idPizzas = ${menuPizza.idPizzas} ` +
-    `JOIN beverages ON beverages.idBeverages = ${menuPizza.idBeverages} ` +
-    `JOIN desserts ON desserts.idDesserts = ${menuPizza.idDesserts} ` +
-    `JOIN pizzasOrders ON pizzasOrders.idPizzas = ${menuPizza.idPizzas} ` +
-    `JOIN beveragesOrders ON beveragesOrders.idBeverages = ${menuPizza.idBeverages} ` +
-    `JOIN dessertsOrders ON dessertsOrders.idDesserts = ${menuPizza.idDesserts} ` +
-    `WHERE isMenuPizz IS NOT NULL AND isMenuDess IS NOT NULL AND isMenuBev IS NOT NULL ` +
-    `GROUP BY pizzName`;
+const sqlRequestDetailsMenu = () => 
+    `SELECT orders.idOrders, pizzName, bevName, dessName, menuQuantity, menuPizzPrice FROM menu `+
+    `JOIN pizzas ON pizzas.idPizzas = menu.idPizzas ` +
+    `JOIN beverages ON beverages.idBeverages = menu.idBeverages ` +
+    `JOIN desserts ON desserts.idDesserts = menu.idDesserts ` + 
+    `JOIN orders ON orders.idOrders = menu.idOrders ` +
+    `WHERE menu.idPizzas IS NOT NULL`;
 
-const detailPizzaMenu = resultsInMenuPizzaData => {
+const detailPizzaMenu = () => {
     return new Promise((resolve, reject) => {
-        const result = [];
-        resultsInMenuPizzaData.map((pizzaMenu, i) => {
-            connection.query(sqlRequestDetailsMenu(pizzaMenu), (err, results) => {
-                if (err) reject([err, 2]);
-                result.push(results[0]);
-                if (i === results.length) resolve(result); 
-            });
+        connection.query(sqlRequestDetailsMenu(), (err, results) => {
+            if (err) reject([err, 2]);
+            
+            resolve(results); 
         });
     });
 };
 
-module.exports = { detailPizzaMenu, menuPizzasData }
+module.exports = { detailPizzaMenu }
