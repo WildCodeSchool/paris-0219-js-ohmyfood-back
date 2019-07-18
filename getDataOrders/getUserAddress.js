981bc7220e18498a7a34e2c02f09d6f0463fa24c
Marc Bernini <bernini.marc@gmail.com>
2019-07-18 18:00:52 +0200

    Add ordersId to know in wich orders this infos going with

--- getDataOrders/getUserAddress.js
+++ getDataOrders/getUserAddress.js
@@ -3,20 +3,23 @@ const connection = require("../conf");
 // ---------------------------------GET USER DETAIL ADRESS------------------------
 
 const sqlRequestUserAddress = userId => 
-    `SELECT userAddress1, userAddress2, zipcode, city, userFacturation, userAddressFacturation FROM userAddress ` +
-    `WHERE idUsers = ${userId}`;
+    `SELECT orders.idOrders, userAddress1, userAddress2, zipcode, city, userFacturation, userAddressFacturation FROM users ` +
+    `JOIN orders ON orders.idUsers = ${userId} ` +
+    `JOIN userAddress ON userAddress.idUsers = ${userId}`;
 
-const userAddressTableData = orderInfo => {
+const userAddressTableData = ordersInfos => {
     return new Promise((resolve, reject) => {
         // Get final result in this array
         const result = [];
-
+        
         // Map to get all infos according to orders
-        orderInfo.map((ordersInfos, i) => {
-            connection.query(sqlRequestUserAddress(ordersInfos.idUsers), (err, results) => {
+        ordersInfos.map((orderInfo, i) => {
+            connection.query(sqlRequestUserAddress(orderInfo.idUsers), (err, results) => {
                 if (err) reject([err, 'userAddressTable']);
                 result.push(results[0]);
-                if (i === results.length) resolve(result);
+                
+                if (i + 1 === ordersInfos.length) 
+                resolve(result);
             });
         });
     });
