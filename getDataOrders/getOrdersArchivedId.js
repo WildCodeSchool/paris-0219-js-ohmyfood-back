05a662f199bdcf9c132a121f8da7baeb0b03ac39
Marc Bernini <bernini.marc@gmail.com>
2019-07-22 15:46:05 +0200

    Add new method to get only orders infos of orders archived by admin

---
+++ getDataOrders/getOrdersArchivedId.js
@@ -0,0 +1,20 @@
+const connection = require("../conf");
+
+// -----------------------------GET ID FROM ORDERS---------------------------
+
+const sqlRequestOrdersTableArchived = () => 
+    'SELECT users.idUsers, firstname, lastname, mail, phoneNumber, deliveryAddress, userAddressFacturation, ' +
+    'idOrders, dateOrder, orderPrice, userMessage, orderMessage FROM users JOIN orders ON orders.idUsers = users.idUsers ' +
+    `JOIN userAddress ON userAddress.idUsers = users.idUsers ` +
+    `WHERE archive IS NOT NULL`;
+
+const ordersTableDataArchived = () => {
+    return new Promise((resolve, reject) => {
+        connection.query(sqlRequestOrdersTableArchived(), (err, results) => {
+            if (err) reject([err, 'OrdersTable']);
+            resolve(results);
+        });
+    });
+};
+    
+module.exports = { ordersTableDataArchived };
