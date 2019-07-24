e7f41663c36d6f1f4938f568b62c162348d9e41e
Arthur Baudel <arthurbaudel75.com>
2019-07-24 15:14:13 +0200

    ongoing work, won't be ready for tomorrow

--- conf.js
+++ conf.js
@@ -2,11 +2,7 @@ const  mysql = require('mysql');
 const  connection = mysql.createConnection({
 host :  'localhost',
 user :  'root',
-<<<<<<< HEAD
 password :  'needkama',
-=======
-password :  '',
->>>>>>> b78a9304305b2d204714bb654c791e240c59662b
 database :  'ohMyFood'
 });
 
--- routes/menusPrice.js
+++ routes/menusPrice.js
@@ -12,24 +12,17 @@ router.get("/", (req, res) => {
   });
 });
 
-/* router.put('/', (req, res) => {
+router.put('/', (req, res) => {
   let menuName = '';
+  
 
-  if(req.body.idTaxName.indexOf('|') != -1) {
-    nameTax = req.body.idTaxName.split('|')[0];
-    req.body.idTaxName = req.body.idTaxName.split('|')[1];
-  } else {
-    nameTax = req.body.idTaxName;
-  }
-  const taxUpdate = req.body;
-
-  connection.query('UPDATE menusPrice SET ? WHERE column = ?', [taxUpdate, nameTax], err => {
+  connection.query(`UPDATE menusPrice SET ${menuName} = ?`, [menuName], err => {
     if (err) {
-      res.status(500).send("Erreur lors de la modification de la tax");
+      res.status(500).send("Erreur lors de la modification du prix du menu");
     } else {
       res.sendStatus(200);
     };
   });
-}); */
+});
 
 module.exports = router;
